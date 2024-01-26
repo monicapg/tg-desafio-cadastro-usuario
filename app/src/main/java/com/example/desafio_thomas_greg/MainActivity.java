ea0aa8140480589288cad1b6ad1db7cc7e2b0778
monicapg <monicapgomides@gmail.com>
2024-01-26 20:26:38 -0300

    feat: duas telas base, lógica a ser implementada.

---
+++ app/src/main/java/com/example/desafio_thomas_greg/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.desafio_thomas_greg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
